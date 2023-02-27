4f82bf399b3286fabb0e62639069d32737e755b6
Kendrick Ngo <kendrickngo200)@github.com>
2023-02-27 12:41:45 -0800

    added basic file structure and README

---
+++ main.py
@@ -0,0 +1,10 @@
+from search import *
+
+
+# YOUR CODE GOES HERE
+if __name__ == '__main__':
+    wgc = WolfGoatCabbage()
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution)
+    solution = breadth_first_graph_search(wgc).solution()
+    print(solution)
