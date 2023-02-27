0f9bba013acdbde18ab3b8dcec02dca91025bb3c
Kendrick Ngo <kendrickngo200)@github.com>
2023-02-27 13:10:28 -0800

    added subclass

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from search import *
-
+from search import Problem
 
 # YOUR CODE GOES HERE
+
+
+class myProblem(Problem):
+
+
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
     solution = depth_first_graph_search(wgc).solution()
